e97caf595cca6d3e966eb7e560d6b2b437618288
Diegantos <diegantos8@gmail.com>
2023-09-12 10:31:45 +0200

    Validation NotNull

--- src/main/java/com/ds/backend/usersapp/backendusersapp/models/entities/User.java
+++ src/main/java/com/ds/backend/usersapp/backendusersapp/models/entities/User.java
@@ -2,6 +2,7 @@ package com.ds.backend.usersapp.backendusersapp.models.entities;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.antlr.v4.runtime.misc.NotNull;
 
 @Entity
 @Table(name = "users")
@@ -10,9 +11,13 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
     @Column(unique = true)
     private String username;
+    @NotNull
     private String password;
+    @NotNull
     @Column(unique = true)
     private String email;
 
